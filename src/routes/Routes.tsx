ebff6c1e32645e8252a79f71c6e8bc67180be102
Nasir Husain <nasirhusain@Nasirs-MacBook-Pro.local>
2022-03-05 23:34:58 +0530

    added loaded on chunk loading

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
 
 import App from "../app/App";
 const NotFound = lazy(() => import("../domains/NotFound"));
@@ -13,7 +14,7 @@ const Movies = lazy(() => import("../domains/Movies/MoviesContainer"));
 const MainRoutes = (): JSX.Element => {
   return (
     <Router>
-      <Suspense fallback={<div>route loading....</div>}>
+      <Suspense fallback={<CircularProgress color="inherit" />}>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<PeopleList />} />
